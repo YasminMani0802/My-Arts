79dcc944e47e3d012bf9eba8d2c5d95495f26cc6
YasminVarshaviak <yasmin1313@gmail.com>
2023-02-06 16:25:03 +0200

    first commit

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -6,12 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeAuthComponent } from './home-auth/home-auth.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 
 
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, HomeAuthComponent],
+  declarations: [LoginComponent, RegisterComponent, HomeAuthComponent, ForgotPasswordComponent, ResetPasswordComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
